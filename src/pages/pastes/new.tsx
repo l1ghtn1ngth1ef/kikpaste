e2d37a379c87f9f80a8f9ec39e5efc41bf7819d1
PerfectProductions <director@perfect.productions>
2023-08-04 22:21:14 +1000

    Awesum Reloadable Editor Form !!! 🚀🍺💣

--- src/pages/pastes/new.tsx
+++ src/pages/pastes/new.tsx
@@ -1,40 +1,59 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-import { Routes } from "@blitzjs/next"
+import React from "react"
 import Link from "next/link"
-import router, { useRouter } from "next/router"
+import { Suspense } from "react"
+import ReactDOM from "react-dom"
+import { stringify } from "flatted"
+import Loadable from "react-loadable"
+import { Routes } from "@blitzjs/next"
+import { useState, useEffect } from "react"
 import { useMutation } from "@blitzjs/rpc"
 import Layout from "src/core/layouts/Layout"
-import { CreatePasteSchema } from "src/pastes/schemas"
-import createPaste from "src/pastes/mutations/createPaste"
-import { PasteForm, FORM_ERROR } from "src/pastes/components/PasteForm"
-import { Suspense } from "react"
-import ClassicEditor from "@ckeditor/ckeditor5-build-classic"
+import router, { useRouter } from "next/router"
 import { CKEditor } from "@ckeditor/ckeditor5-react"
-import { useState, useEffect } from "react"
-import Loadable from "react-loadable"
-import { stringify } from "flatted"
-import DecoupledEditor from "@ckeditor/ckeditor5-build-decoupled-document"
-import React from "react"
-import ReactDOM from "react-dom"
+import { CreatePasteSchema } from "src/pastes/schemas"
 import { Form, FormProps } from "src/core/components/Form"
+import createPaste from "src/pastes/mutations/createPaste"
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic"
+import { PasteForm, FORM_ERROR } from "src/pastes/components/PasteForm"
 import { LabeledTextField } from "src/core/components/LabeledTextField"
+import DecoupledEditor from "@ckeditor/ckeditor5-build-decoupled-document"
+import { useRef } from "react"
 
 // @ts-check
 function NewPastePage() {
+  const editorRef = useRef()
+  const [editorLoaded, setEditorLoaded] = useState(false)
+  const { CKEditor, DecoupledEditor } = editorRef.current || {}
+  const [data, setData] = useState("")
+  const [values, setValue] = useState("")
+
   const router = useRouter()
   const [CreatePasteMutation] = useMutation(createPaste)
+  const [textEditor, setTextEditor] = useState("")
 
   const handleSubmit = async (values) => {
     const paste = await CreatePasteMutation(values)
     await router.push(Routes.ShowPastePage({ pasteId: paste.id }))
     console.log("Pressed Button!")
+    console.log("Values!", values)
+    console.log("paste Value!", values.target.value)
+    setValue(values.target.value)
   }
 
   const inputHandler = (event, editor) => {}
 
+  useEffect(() => {
+    editorRef.current = {
+      CKEditor: require("@ckeditor/ckeditor5-react").CKEditor, //Added .CKEditor
+      DecoupledEditor: require("@ckeditor/ckeditor5-build-decoupled-document"),
+    }
+    setEditorLoaded(true)
+  }, [])
+
   return (
-    <div className="Submit">
-      <div>
+    <>
+      {editorLoaded ? (
         <Form onSubmit={handleSubmit}>
           <LabeledTextField
             name="pasteName"
@@ -49,6 +68,7 @@ function NewPastePage() {
             type="text"
           />
           <LabeledTextField
+            className={values && "filled"}
             name="pasteContent"
             label="Paste Content"
             placeholder="Paste Content"
@@ -57,22 +77,28 @@ function NewPastePage() {
 
           {/* template: <__component__ name="__fieldName__" label="__Field_Name__" placeholder="__Field_Name__"  type="__inputType__" /> */}
 
-          <div>
-            <CKEditor
-              id="inputText"
-              editor={DecoupledEditor}
-              onChange={inputHandler}
-              schema={CreatePasteSchema}
-              onSubmit={async (values) => {
-                const data = editor.getData()
-                setTextEditor(data)
-              }}
-            />
-          </div>
+          <CKEditor
+            editor={DecoupledEditor}
+            data={data}
+            onReady={(editor: any) => {
+              console.log("Editor is ready to use!", editor)
+            }}
+            onChange={(event, editor) => {
+              const data = editor.getData()
+              console.log("getData!", stringify(event))
+
+              setData(data)
+
+              console.log("setData!", stringify(event))
+              console.log("Data: ", data)
+            }}
+          />
           <button type="submit">Submit!</button>
         </Form>
-      </div>
-    </div>
+      ) : (
+        <p>Loading Incredible Website Extension...</p>
+      )}
+    </>
   )
 }
 
