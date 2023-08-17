b2ce6acb44177b85bb70ae3e87daa10ff3b107c4
PerfectProductions <director@perfect.productions>
2023-08-18 06:59:07 +1000

    ⚡ ⚡ ⚡ Perfected! ⚡ ⚡ ⚡

--- src/blitz-server.ts
+++ src/blitz-server.ts
@@ -1,17 +1,24 @@
-import { setupBlitzServer } from '@blitzjs/next';
-import { AuthServerPlugin, PrismaStorage } from '@blitzjs/auth';
-import { simpleRolesIsAuthorized } from '@blitzjs/auth';
-import { BlitzLogger } from 'blitz';
-import db from 'db';
-import { authConfig } from './blitz-client';
+import { setupBlitzServer } from "@blitzjs/next"
+import { AuthServerPlugin, PrismaStorage } from "@blitzjs/auth"
+import { simpleRolesIsAuthorized } from "@blitzjs/auth"
+import { BlitzLogger } from "blitz"
+import db from "db"
+import { authConfig } from "./blitz-client"
 
-export const { gSSP, gSP, api } = setupBlitzServer({
-	plugins: [
-		AuthServerPlugin({
-			...authConfig,
-			storage: PrismaStorage(db),
-			isAuthorized: simpleRolesIsAuthorized
-		})
-	],
-	logger: BlitzLogger({})
-});
+const { gSSP, gSP, api } = setupBlitzServer({
+  plugins: [
+    AuthServerPlugin({
+      cookiePrefix: "blitz",
+      storage: PrismaStorage(db),
+      isAuthorized: simpleRolesIsAuthorized,
+      sameSite: "none",
+      secureCookies: true,
+    }),
+  ],
+  formatError: (error) => {
+    return new Error("Formatted error" + error.message)
+  },
+  logger: BlitzLogger({}),
+})
+
+export { gSSP, gSP, api }
